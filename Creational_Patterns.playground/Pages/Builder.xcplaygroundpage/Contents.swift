ed5243ec71181f7345e9cfcace8e9a80468cc25c
Joan Barrull Ribalta <giria@users.noreply.github.com>
2022-08-06 19:47:40 +0100

    Factory

---
+++ Creational_Patterns.playground/Pages/Builder.xcplaygroundpage/Contents.swift
@@ -0,0 +1,58 @@
+//: [Previous](@previous)
+/*:
+ 
+ The intent of the Builder design pattern is to separate the construction of a complex object from its representation.
+  ![builder](builder.png)
+ */
+
+func createBuilder(configuration: [String: String]) -> LaptopBuilder? {
+    guard let model = configuration["base_model"] else {
+        return nil
+    }
+        
+    var laptopBuilder: LaptopBuilder?
+    switch model {
+    case "budget":
+        laptopBuilder = BudgetLaptopBuilder()
+    case "office":
+        laptopBuilder = OfficeLaptopBuilder()
+    case "high-end":
+        laptopBuilder = HighEndLaptopBuilder()
+    default:
+        print("Unexpected value.")
+    }
+    
+    return laptopBuilder
+}
+
+var configuration = ["base_model": "budget"]
+var laptopBuilder = createBuilder(configuration: configuration)
+var director = Director(builder: laptopBuilder)
+director.constructLaptop(configuration: configuration)
+
+if let laptop = laptopBuilder?.getLaptop() {
+    print(laptop)
+}
+
+configuration = ["base_model": "office",
+                 "size": "13-inch"]
+laptopBuilder = createBuilder(configuration: configuration)
+director = Director(builder: laptopBuilder)
+director.constructLaptop(configuration: configuration)
+
+if let laptop = laptopBuilder?.getLaptop() {
+    print(laptop)
+}
+
+configuration = ["base_model": "high-end",
+                 "size": "13-inch",
+                 "graphics": "intel-iris-plus-graphics-645"]
+laptopBuilder = createBuilder(configuration: configuration)
+director = Director(builder: laptopBuilder)
+director.constructLaptop(configuration: configuration)
+
+if let laptop = laptopBuilder?.getLaptop() {
+    print(laptop)
+}
+
+//: [Next](@next)
---
+++ Creational_Patterns.playground/Pages/Factory.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+//: [Previous](@previous)
+
+
+
+/*:
+ 
+   Decouples the caller from concrete types.
+ Promotes loose coupling
+ Two variants:
+ - Create subclass per Product type
+ - Parametrized Factory Method
+ 
+ */
+import Foundation
+
+var greeting = "Hello, playground"
+
+//: [Next](@next)
