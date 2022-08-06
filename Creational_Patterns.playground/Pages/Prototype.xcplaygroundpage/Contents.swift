a167f97a24a33bbcd46ca4577ab6905703695ff9
Joan Barrull Ribalta <giria@users.noreply.github.com>
2022-08-06 14:03:30 +0100

    Proto

--- Creational_Patterns.playground/Pages/Prototype.xcplaygroundpage/Contents.swift
+++ Creational_Patterns.playground/Pages/Prototype.xcplaygroundpage/Contents.swift
@@ -4,7 +4,7 @@
  
  The prototype works by cloning an existing prototype object. We instantiate the prototype instance as usual; however, all consecutive instances get created by copying this prototype. As a result, we get rid of the repeated expansive initialization code for all successive object creations. We rely on cloning, which is more efficient. 
  
+ 
+ In Swift value types (structs, enums and tuples) get copied by default.
+ Classes can be copied conforming NSCopying protocol and implementing deep copy.
  */
-import UIKit
-
-var greeting = "Hello, playground"
